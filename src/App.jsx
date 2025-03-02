df21e19dbb854d7ab2e7d861ed3321e759d5752f
NazarKopach <164062094+NazarKopach@users.noreply.github.com>
2025-03-02 17:40:12 +0100

    correct error

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import HomePages from "./pages/HomePages/HomePages.jsx";
 import CatalogPages from "./pages/CatalogPages/CatalogPages.jsx";
 import InfoPages from "./pages/InfoPage/InfoPages.jsx";
 import SvgSprite from "./components/SvgSprite/SvgSprite.jsx";
+import Header from "./components/Header/Header.jsx";
 
 import { Route, Routes } from "react-router-dom";
-import Header from "./components/header/header.jsx";
 
 function App() {
   return (
